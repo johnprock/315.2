256cb2d184ed5a9d2a68055edff663156a4d5d2e
josejlm2 <josejlm2@hotmail.com>
2013-10-09 16:07:25 -0500

    initial coding. draws simple board.

---
+++ board/Board.java
@@ -0,0 +1,24 @@
+
+public class Board {
+
+
+	public static void DrawBoard()
+	{
+		System.out.println("  _ _ _ _ _ _ _ _");
+		System.out.println("1|_|_|_|_|_|_|_|_|");
+		System.out.println("2|_|_|_|_|_|_|_|_| ");
+		System.out.println("3|_|_|_|_|_|_|_|_| ");
+		System.out.println("4|_|_|_|_|_|_|_|_| ");
+		System.out.println("5|_|_|_|_|_|_|_|_| ");
+		System.out.println("6|_|_|_|_|_|_|_|_| ");
+		System.out.println("7|_|_|_|_|_|_|_|_| ");
+		System.out.println("8|_|_|_|_|_|_|_|_| ");
+		System.out.println("  a b c d e f g h  ");
+	}
+
+
+    public static void main(String[] args) {
+        DrawBoard();
+    }
+
+}
