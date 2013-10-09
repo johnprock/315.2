ddd8d9837fdf23f0f71d9b6e47d55b793ddeebde
Patrick Rock <prock2011@gmail.com>
2013-10-08 22:28:59 -0500

    worked on state

--- src/State.java
+++ src/State.java
@@ -1,3 +1,27 @@
 public class State {
   Piece[][] board; 
+
+  // regular constructor
+  State() {
+    board = new Piece[8][8];
+  }
+
+  // copy constructor
+  State(State state) {
+    this.board = state.board;
+  }
+
+  Piece getPiece(int x, int y) {
+    return board[x][y];
+  }
+
+  void flipPiece(int x, int y) {
+  }
+
+  void addBlack(int x, int y) {
+  }
+
+  void addWhite(int x, int y) {
+  }
+ 
 }
