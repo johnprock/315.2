4a6b648a1d12e86daaba892b56a6a70bbbf3b501
Patrick Rock <prock2011@gmail.com>
2013-10-13 21:59:34 -0500

    engine code done for move

--- src/Engine.java
+++ src/Engine.java
@@ -1,19 +1,43 @@
 public class Engine {
   State state;
+  Boolean turn;
   
   Engine() {
     state = new State();
+    turn = true; // dark player moves first
+
   }
 
-  public void move(Location loc) {
+  public Boolean move(Location _loc) {
+    if(!turn) { // move black player
+      Piece p = new BlackPiece(_loc);
+      if(state.isValidMove(p)) {
+        state.addBlack(_loc.getX(), _loc.getY());
+        return true;
+      }
+    }
+    else { // move white player
+      Piece p = new WhitePiece(_loc);
+      if(state.isValidMove(p)) {
+        state.addWhite(_loc.getX(), _loc.getY());
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public Boolean isOver() {
+    return state.isOver(turn);
   }
 
   public void aiMove() {
+    if(turn) { // black
+
+    }
+    else { // white
+
+    }
+
   }
 
-  public void playHuman() {
-  }
-
-  public void playAI() {
-  }
 }
