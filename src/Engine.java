0c1217c8fa0d6da4e894f86ebb8c15a021d2b6a6
Patrick Rock <prock2011@gmail.com>
2013-10-13 22:11:23 -0500

    integrated board drawing code;

--- src/Engine.java
+++ src/Engine.java
@@ -41,4 +41,28 @@ public class Engine {
 
   }
 
+  public void DrawBoard(State currentState) {
+    String newLine = System.getProperty("line.separator");
+    System.out.println("  _ _ _ _ _ _ _ _");//top of the board
+
+    for (int row = 0; row<8; row++){
+      System.out.print(row+"|");
+      for (int column = 0; column<8; column++)
+      {
+        if (currentState.board[row][column]!= null){
+          if (currentState.getPiece(row,column).isBlack()){
+            System.out.print("@|");
+          }
+          if (currentState.getPiece(row,column).isWhite()){
+            System.out.print("O|");
+          }
+        }
+        else
+          System.out.print("_|"); 
+      }
+      System.out.println("");
+    }
+    System.out.println("  a b c d e f g h");//bottom of the board
+  }
+
 }
