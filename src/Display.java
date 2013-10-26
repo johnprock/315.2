6a9e2a27cfaadcd0f79ea5b2c45f4b6a78483c45
Patrick Rock <prock2011@gmail.com>
2013-10-26 17:42:05 -0500

    draws pieces

--- src/Display.java
+++ src/Display.java
@@ -29,6 +29,7 @@ public class Display extends JFrame {
       super();
       Dimension d = new Dimension(1000,1000);
       setPreferredSize(d);
+      state = new State();
     }
 
     public void paintComponent(Graphics g) {
@@ -38,11 +39,40 @@ public class Display extends JFrame {
     public void drawBoard(Graphics g) {
       int width = 50;    
 
+      // add squares
       for(int row=0; row<8; row++) {
         for(int col=0; col<8; col++) {
+ 
           drawSquare(g, row*width, col*width, width);
+          if(state.getPiece(row,col).isBlack()) {
+            drawPiece(g, row*width, col*width, width, 1);
+          }
+          else if(state.getPiece(row,col).isWhite()) {
+            drawPiece(g, row*width, col*width, width, 2);
+          }
+          else if(state.getPiece(row,col).isEmpty()) {
+            drawPiece(g, row*width, col*width, width, 3);
+          }
         }
       } 
+
+    }
+
+    public void drawPiece(Graphics g, int x, int y, int width, int color) {
+        // 1 - black
+        // 2 - white
+        // 3 - empty
+
+        if(color == 1) {
+          g.setColor(Color.black);
+          g.fillOval(x+5, y+5, width-10, width-10);
+        }
+        else if(color == 2) {
+          g.setColor(Color.white);
+          g.fillOval(x+5, y+5, width-10, width-10);
+        }
+        else if(color == 3) {
+        }
 
     }
 
@@ -71,6 +101,7 @@ public class Display extends JFrame {
       state = _state;
     }
   }
+
 
   private class MenuBar extends JMenuBar {
     
