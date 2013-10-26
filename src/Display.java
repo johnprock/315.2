12ac70ab97a25c6cc008185c43561af0d197c4af
Patrick Rock <prock2011@gmail.com>
2013-10-26 17:29:11 -0500

    draws board

--- src/Display.java
+++ src/Display.java
@@ -16,31 +16,67 @@ public class Display extends JFrame {
     setJMenuBar(menuBar);
     getContentPane().add(board, BorderLayout.CENTER);
 
-    setSize(300,500);
     pack();
     setVisible(true);
 
   }
 
   private class Board extends JComponent {
-    
+
+    State state;    
+
     Board() {
       super();
-      Dimension d = new Dimension(300,300);
+      Dimension d = new Dimension(1000,1000);
       setPreferredSize(d);
     }
 
     public void paintComponent(Graphics g) {
-      g.setColor(Color.black);
-      g.drawRect(0,0,20,20);
+      drawBoard(g);
     } 
+
+    public void drawBoard(Graphics g) {
+      int width = 50;    
+
+      for(int row=0; row<8; row++) {
+        for(int col=0; col<8; col++) {
+          drawSquare(g, row*width, col*width, width);
+        }
+      } 
+
+    }
+
+    public void drawSquare(Graphics g, int x, int y, int width) {
+        int borderWidth = width/20;
+
+        // draw green square
+        g.setColor(Color.green);
+        g.fill3DRect(x, y, width, width, false);
+        
+        // draw left border
+        g.setColor(Color.black);
+        g.fill3DRect(x, y, borderWidth, width, false);
+       
+        // draw right border
+        g.fill3DRect(x+width-borderWidth, y, borderWidth,  width, false);    
+
+        // draw top border
+        g.fill3DRect(x, y, width, borderWidth, false);
+        
+        // draw bottom border
+        g.fill3DRect(x, y+width-borderWidth, width, borderWidth, false);
+    }
+
+    public void updateState(State _state) {
+      state = _state;
+    }
   }
 
   private class MenuBar extends JMenuBar {
     
     MenuBar() {
       super();
-      JMenu menu = new JMenu("A Menu");
+      JMenu menu = new JMenu("Options");
       add(menu);
     }
   }
