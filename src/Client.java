8d8b40ddaf2c62b027cd242adefaea1d1fe389eb
FelixLOL <liyaofei@tamu.edu>
2013-10-28 00:09:09 +0800

    change the client for using
    only can show single line on terminal, but for GUI, it doesnot matter

--- src/Client.java
+++ src/Client.java
@@ -7,43 +7,65 @@ public class Client{
 
 	Display d = new Display("test");
     
-
+	Socket socket;
+    PrintWriter out;
+    BufferedReader br;
+	
+	public void connect(String host, int portNumber){
+		String userInput;
+		try{
+            System.out.println("Creating socket to '" + host + "' on port " + portNumber);
+			socket = new Socket(host, portNumber);
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
+			System.out.println(br.readLine());
+			while(true){
+			
+				Scanner in = new Scanner(System.in);
+				userInput = in.nextLine();
+				 if ("exit".equalsIgnoreCase(userInput)) {
+					 socket.close();
+					 break;
+				 }
+				//br.readLine();
+				out.println(userInput);
+				System.out.println(br.readLine());
+				//String sCurrentLine= null;
+				//while((sCurrentLine =br.readLine()) != null){
+						//System.out.println(sCurrentLine );
+				//	}
+			}
+			
+			
+		} catch (UnknownHostException err) {
+			err.printStackTrace();
+		} catch (IOException err) {
+			err.printStackTrace();
+		}
+	
+	}
 	public static void main(String args[]){
-		ServerParser parser;
-        parser = new ServerParser();
-
+		
 		System.out.println("Server IP: ");
 		Scanner in = new Scanner(System.in);
 		final String host = in.nextLine();
 		//final String host = "localhost";
 		final int portNumber = 8123;
-	    try{
-            System.out.println("Creating socket to '" + host + "' on port " + portNumber);
+	    Client client=new Client();
+		client.connect(host,portNumber);
 
-        // main loop
-		while (true) {
-			Socket socket = new Socket(host, portNumber);
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			// BufferedReader userInputBR = new BufferedReader(new InputStreamReader(System.in));
+			// String userInput = userInputBR.readLine();
 
-			System.out.println("server says:" + br.readLine());
+			// out.println(userInput);
 
-			BufferedReader userInputBR = new BufferedReader(new InputStreamReader(System.in));
-			String userInput = userInputBR.readLine();
+			// System.out.println("server says:" + br.readLine());
 
-			out.println(userInput);
-
-			System.out.println("server says:" + br.readLine());
-
-			if ("exit".equalsIgnoreCase(userInput)) {
-				socket.close();
-				break;
-		    	}
-	    	}
-        }catch (UnknownHostException err) {
-			err.printStackTrace();
-		} catch (IOException err) {
-			err.printStackTrace();
-		}
+			// if ("exit".equalsIgnoreCase(userInput)) {
+				// socket.close();
+				// break;
+		    	// }
+	    	// }
+       
 	}
 }
